449087bdf8913f2c955f309e7692d94492537495
Никита Батуев <sellerbto@MacBook-Pro-Nikita.local>
2025-05-25 23:08:04 +0500

    add ci-cd

--- api/auth.py
+++ api/auth.py
@@ -1,15 +1,17 @@
 from fastapi import Request, HTTPException, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+from app.models import User
 from .database import db
 
 security = HTTPBearer()
 
-async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)) -> str:
+async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)) -> User:
     if not credentials.scheme == "TOKEN":
         raise HTTPException(status_code=401, detail="Invalid authentication scheme")
-    
+
     user = db.get_user_by_api_key(credentials.credentials)
     if not user:
         raise HTTPException(status_code=401, detail="Invalid token")
-    
-    return user 
+
+    return user
